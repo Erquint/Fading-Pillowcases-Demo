0fda17723151d4939d77734fbc0707496b3d5316
Gness Erquint <gness.na@gmail.com>
2020-07-23 05:36:02 +0300

    Refactored by Johnicholas Hines.

--- app/main.rb
+++ app/main.rb
@@ -1,62 +1,156 @@
-class Fader
-  attr_accessor :solid, :fresh
-  @@faders = []
-  
-  def initialize solid
-    @solid = solid
-    @fresh = true
-    @solid[:a] = 55
-    @@faders << self
+class FaderParticle
+    def initialize solid
+        @x = solid.x
+        @y = solid.y
+        @w = solid.w
+        @h = solid.h
+        @a = 55
+        @fresh = true
+    end
+    
+    def alive
+        @a > 0
+    end
+    
+    def update inputs
+        if @fresh then
+            @a += 5
+        else
+            @a -= 1
+        end
+        
+        if @a > 254 then
+          @fresh = false
+        end
+    end
+    
+    def draw outputs
+        outputs.solids << {
+            x: @x,
+            y: @y,
+            w: @w,
+            h: @h,
+            r: 255,
+            g: 0,
+            b: 0,
+            a: @a
+        }
+    end
+    
+    def tick args
+        update args.inputs
+        draw args.outputs
+    end
+end
+
+class ParticleGroup
+  def initialize
+    @particles = []
   end
   
-  def self.live
-      @@faders.select! {|fader| fader.solid[:a] > 1} # Culling the invisible faders.
-      @@faders.each do |fader| # Fader lifecycle.
-        if fader.fresh then fader.solid[:a] += 5 else fader.solid[:a] -= 1 end
-        if fader.solid[:a] > 254 then fader.fresh = false end
-      end
-      nil
+  def add p
+    @particles << p
   end
   
-  def self.solids
-    @@faders.map(&:solid)
+  def update inputs
+    # Cull the dead entities
+    @particles.select! { |p| p.alive }
+    @particles.each { |p| p.update inputs }
+  end
+  
+  def draw outputs
+    @particles.each { |p| p.draw outputs }
+    
+    # debugging
+    outputs.labels << {
+      x: 20,
+      y: 20,
+      text: 'Particles: ' + @particles.size.to_s
+    }
+  end
+  
+  def tick args
+    update args.inputs
+    draw args.outputs
   end
 end
 
-def tick args
-  args.state.solids.pillowcase ||= {}
-  
-  if args.inputs.mouse.down # Tracking mouse drag initial position…
-    args.state.inputs.mouse.held.point ||= args.inputs.mouse.down.point
-  end
-  if args.inputs.mouse.up # …and whether it's currently held.
-    args.state.inputs.mouse.held.point = nil
-    
-    if args.state.solids.pillowcase # Making the current pillowcase a new fader.
-      Fader.new args.state.solids.pillowcase
-      args.state.solids.pillowcase = nil
-    end
+class Pillowcase
+  def initialize initial
+    @x = initial.x
+    @y = initial.y
+    @w = 0
+    @h = 0
   end
   
-  Fader.live
-  args.outputs.solids << Fader.solids
-  
-  if args.state.inputs.mouse.held.point # Setting the current pillowcase.
-    args.state.solids.pillowcase = {
-      x: args.state.inputs.mouse.held.point.x,
-      y: args.state.inputs.mouse.held.point.y,
-      w: args.inputs.mouse.point.x - args.state.inputs.mouse.held.point.x,
-      h: args.inputs.mouse.point.y - args.state.inputs.mouse.held.point.y,
+  def solid
+    {
+      x: @x,
+      y: @y,
+      w: @w,
+      h: @h,
       r: 255,
       g: 0,
       b: 0,
       a: 55,
-      fresh: true
     }
   end
   
-  args.outputs.solids << args.state.solids.pillowcase
+  def update inputs
+    @w = inputs.mouse.point.x - @x
+    @h = inputs.mouse.point.y - @y
+  end
   
-  # args.outputs.labels << [20, 20, 'Faders ' + Fader.solids.size.to_s]
-  # To make sure they do in fact get culled and not just invisible.
+  def draw outputs
+    outputs.solids << solid
+  end
+  
+  def tick args
+    update args.inputs
+    draw args.outputs
+  end
+end
+
+class FadersAndPillowcase
+  def initialize
+    @faders = ParticleGroup.new
+    @pillowcase = nil
+  end
+  
+  def update inputs
+    @faders.update inputs
+      
+    if inputs.mouse.down and @pillowcase == nil then
+      @pillowcase = Pillowcase.new inputs.mouse.down.point
+    end
+    
+    if @pillowcase then
+      @pillowcase.update inputs
+    end
+    
+    if inputs.mouse.up and @pillowcase != nil then
+      @faders.add FaderParticle.new @pillowcase.solid
+      @pillowcase = nil
+    end
+  end
+  
+  def draw outputs
+    @faders.draw outputs
+    
+    if @pillowcase then
+      @pillowcase.draw outputs
+    end
+  end
+  
+  def tick args
+    update args.inputs
+    draw args.outputs
+  end
+end
+
+$effect = FadersAndPillowcase.new
+
+def tick args
+  $effect.update args.inputs
+  $effect.draw args.outputs
 end
